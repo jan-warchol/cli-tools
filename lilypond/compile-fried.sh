27da23a55c61e23fe78c3b436730708c020ac769
Janek Warchoł <lemniskata.bernoullego@gmail.com>
2013-02-07 00:00:49 +0100

    add script for compiling fried songs.
    despite the modification date being 2013-02-07,
    this version is somewhat from october 2012.
    I suppose it works, but i don't know whether
    the results are desired.

---
+++ lilypond/compile-fried.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# first argument - opus pattern to use
+if [ "$1" != "" ]; then opus=$1; else opus="0[1-4]"; fi
+
+# second argument - lily version to use
+if [ "$2" != "" ]; then lily=$2; else lily=$defaultlily; fi
+
+echo "========================================"
+
+for file in $(find $opus* -name "*standalone.ly"); do
+    echo "compiling $file with $lily..."
+    ~/lily-builds/$lily/out/bin/lilypond --loglevel=ERROR $file
+    echo "----------------------------------------"
+done
+
+ext="pdf"
+suffix=" $lily"
+
+for file in *.$ext; do
+   mv "$file" "${file%%.$ext}$suffix.$ext"
+done
+
+rm *.midi
+mkdir -p ../$lily
+mv *.$ext ../$lily
