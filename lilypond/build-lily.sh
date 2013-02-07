2adf717ba16e279153de6af8ebd2bb1631888779
Janek Warchoł <lemniskata.bernoullego@gmail.com>
2013-02-07 18:24:19 +0100

    build-lily: cosmetics. add some comments.

--- lilypond/build-lily.sh
+++ lilypond/build-lily.sh
@@ -8,18 +8,18 @@
 # 4. building.
 
 
-die() { # helper function
+die() { # in case of some error...
     #aplay -q ~/src/sznikers.wav
-    echo "something went wrong. exiting."
+    echo -e "\e[00;31mSomething went wrong. Exiting.\e[00m"
     exit 1
 }
 
 if [[ "$1" == "help" || "$1" == "-help"
 || "$1" == "--help" || "$1" == "-h"  || "$1" == "h" ]]; then
-    echo "\$1 = operating 'mode' (e.g. to make from Scratch)"
+    echo "\$1 = operating 'mode' (e.g. to make from scratch)"
     echo "\$2 = build directory (default: $LILYPOND_BUILD_DIR)"
     echo "\$3 = commit to build (default: HEAD in $LILYPOND_GIT)"
-    die
+    exit
 fi
 
 echo "========================================"
@@ -62,7 +62,7 @@ if [[ $build == $LILYPOND_GIT/* ]]; then
     source=$LILYPOND_GIT
 else
     building_inside_main_repo="no"
-    source=$build
+    source=$build # (source code will be copied to $build)
 fi
 
 # what we know at this moment:
@@ -78,7 +78,7 @@ fi
 
 # if we're building directly in $LILYPOND_GIT, we don't want
 # to make from scratch (e.g. erase everything before building)
-# because that'd delete the repository and all work done with it.
+# because that'd delete the repository and all of user's work.
 cd $build
 if [[ "$1" == s* && "$build" != "$LILYPOND_GIT" ]]; then
     echo "You requested to build from scratch."
@@ -93,15 +93,16 @@ if [[ "$1" == s* && "$build" != "$LILYPOND_GIT" ]]; then
     mkdir -p $build
 fi
 
-# Check whether $source is inside a repository (and discard actual
-# error message produced by rev-parse by sending it to dev/null).
+# Does $source contain lilypond source code, or is it empty?
+# We check this by asking git if $source is a repository at all.
+# We discard actual error message produced by rev-parse.
 cd $source
 git rev-parse 2> /dev/null
 if [ $? != 0 ]; then
     # non-zero exit status of rev-parse means that this directory
-    # (i.e. $source) isn't inside a (presumably lilypond) repository.
-    # (This implies we're building outside $LILYPOND_GIT,
-    # which means that $source=$build.)
+    # (i.e. $source) isn't inside a repository.
+    # (This means we must be building outside $LILYPOND_GIT,
+    # which in turn means that $source=$build.)
     # 
     # One thing we should check: maybe $build is a directory
     # containing *subdirectories* with lilypond builds?
@@ -121,7 +122,7 @@ if [ $? != 0 ]; then
     done
     if [ $notArepo == 0 ]; then
         echo "You told me to build lilypond inside $build."
-        echo "But it already contains *subdirectories* with lily repositories."
+        echo "But it already contains *subdirectories* with some repositories."
         echo "Are you sure that you want to overwrite them [y/n]?"
         read decision
         if [ "$decision" != "y" ]; then die; fi
@@ -131,8 +132,8 @@ if [ $? != 0 ]; then
     git clone $LILYPOND_GIT $source
     cd $source
 else 
-    # we had sources, but they may need updating
-    # (if they're a local clone of LILYPOND_GIT)
+    # we have lily source code, but it may need updating
+    # (if it's a local clone of LILYPOND_GIT)
     if [[ "$building_inside_main_repo" == "no" ]]; then
         git fetch
     fi
@@ -142,13 +143,17 @@ fi
 # if it wasnt specified by user, grab HEAD of LILYPOND_GIT
 if [ "$3" != "" ]; then
     cd $source
-    git checkout --quiet $3 || die;
+    git checkout --quiet $3
+    || echo "I cannot checkout $3. Maybe it is a detached HEAD?"
+    && echo "Having this commit as an explicit branch might help." && die;
 else
     cd $LILYPOND_GIT
 fi
 commit=$(git rev-parse HEAD)
 cd $source
-git checkout --quiet $commit || die;
+git checkout --quiet $commit
+|| echo "I cannot checkout $commit. Maybe it is a detached HEAD?"
+&& echo "Having this commit as an explicit branch might help." && die;
 
 
 
@@ -159,8 +164,8 @@ echo -e "Attempting to build lilypond: \n"
 git log -n 1
 echo ""
 echo -e "inside directory \n  $build \n"
-# give the user 5 seconds to check above information.
-read -t 10 confirmation
+# give the user 8 seconds to check above information.
+read -t 8 confirmation
 
 # build dir might have been freshly created, so we need
 # to check whether it contains necessary setup information.
