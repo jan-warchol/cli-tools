cdc698699453956d362db4f20f09c16ebd87dc78
Janek Warchoł <lemniskata.bernoullego@gmail.com>
2013-02-07 13:43:38 +0100

    pull-all: rebase on master if upstream not configured
    'git rebase' by default tries to rebase against tracked
    upstream branch, i.e. a remote branch specified in git config
    as the "parent" of current branch. If that branch is not
    defined in config, rebase aborts. Until now this caused the
    whole script to abort as well.
    
    However, since usually the tracked branch should be
    origin/master anyway, pull-all now attempts to rebase against
    origin/master if there's no information about tracked branch in
    config, and aborts only when this attepmt fails as well.

--- lilypond/pull-all.sh
+++ lilypond/pull-all.sh
@@ -21,8 +21,20 @@ echo -e "\e[00;32mUPDATING YOUR BRANCHES\e[00m------------------";
 for branch in $(git branch --color=never | sed s/*//); do
     git checkout --quiet "$branch" || die;
     echo -e "On branch \e[00;33m$branch\e[00m"
-    #rebases on respective remote tracking branch (taken from config)
-    git rebase || die;
+
+    # Try to rebase against respective upstream branch
+    # (taken from config). If that fails, try to rebase
+    # against origin/master, because that's usually what
+    # you want anyway.
+    git rebase
+    if [ $? != 0 ]; then
+        echo -e "\nThis means\e[00;31m a warning: upstream branch is not configured.\e[00m"
+        echo "Don't worry, that's not dangerous."
+        echo "Trying to rebase on origin/master..."
+        sleep 7
+        git rebase origin/master || die;
+        sleep 3
+    fi
     echo "";
 done
 
