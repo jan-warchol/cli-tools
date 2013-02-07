f78d9b07e800c16bb0a8e0a73bf41cd093b07f76
Janek Warchoł <lemniskata.bernoullego@gmail.com>
2013-02-07 13:19:50 +0100

    pull-all: cosmetics, style and colorize.

--- lilypond/pull-all.sh
+++ lilypond/pull-all.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# this will update all branches in lilypond git repository.
+# this will update all branches in LILYPOND_GIT repository.
 # if no argument is specified, it will also delete merged branches.
 
-die() {
-    aplay -q ~/src/sznikers.wav
+die() { # in case of some error...
+    #aplay -q ~/src/sznikers.wav
+    echo -e "\e[00;31mSomething went wrong. Exiting.\e[00m"
     exit 1
 }
 
@@ -12,13 +13,14 @@ currentbranch=$(git branch --color=never | sed --quiet 's/* \(.*\)/\1/p')
 
 cd $LILYPOND_GIT/
 echo "========================================"
-echo "FETCHING--------------------------------"
+echo -e "\e[00;32mFETCHING\e[00m--------------------------------"
 git fetch
 echo ""
 
-echo "UPDATING BRANCHES-----------------------";
+echo -e "\e[00;32mUPDATING YOUR BRANCHES\e[00m------------------";
 for branch in $(git branch --color=never | sed s/*//); do
     git checkout --quiet "$branch" || die;
+    echo -e "On branch \e[00;33m$branch\e[00m"
     #rebases on respective remote tracking branch (taken from config)
     git rebase || die;
     echo "";
@@ -27,7 +29,7 @@ done
 # $# = number of arguments specified by user
 if [ $# == 0 ]; then
     git checkout --quiet master;
-    echo "DELETING MERGED BRANCHES----------------";
+    echo -e "\e[00;32mDELETING MERGED BRANCHES\e[00m----------------";
     for branch in $(git branch --color=never | sed s/*// | sed s/master//); do
         git branch -d "$branch"
         echo ""
