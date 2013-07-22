f5ee7a7a375497502b2b942090e617567966e4dc
Janek Warchoł <lemniskata.bernoullego@gmail.com>
2013-07-22 14:04:49 +0200

    imrpove cpu testing script

--- scripts/cpu-stress.sh
+++ scripts/cpu-stress.sh
@@ -1,14 +1,29 @@
 ﻿#!/bin/sh
 
-# create a file 'testfile' using
-# dd if=/dev/urandom of=testfile count=20 bs=1024k
-# and run this script
+# create a 'testfile' containing random data
+# and repeatedly calculate md5 hash of this file.
 
-i=0
- 
-while [ 1 ]
+echo "Creating a test file..."
+dd if=/dev/urandom of=testfile count=20 bs=1024k
+echo "Test file created."
+echo " "
+sleep 2
+echo "Testing. Please don't perform other CPU-consuming tasks."
+
+i=1
+count=100
+
+START=$(date +%s.%N)
+
+while [ $i -le $count ]
 do
-   md5sum testfile
-   i=`expr $i + 1`
    echo "Iteration: $i"
+   md5sum testfile >> /dev/null
+   i=`expr $i + 1`
 done
+
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+
+echo "Time spent doing $count iterations: $DIFF seconds."
+rm testfile
