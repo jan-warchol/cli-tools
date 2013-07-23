b406f47cf7cf7833f4003303b1d6bd345a0ea1d9
Janek Warchoł <lemniskata.bernoullego@gmail.com>
2013-07-23 19:54:00 +0200

    cleanup in prompts

--- config/bash-prompts.sh
+++ config/bash-prompts.sh
@@ -1,7 +1,7 @@
 # import a file contating nice features for git:
 source "$bashsettings/git-prompt.sh"
 
-# colors:
+# some colors:
 thinblue="\[$(tput setaf 6)\]"
 thickcyan="\[\e[1;36m\]"
 thickblue="\[\e[1;34m\]"
@@ -10,9 +10,9 @@ normal="\[$(tput sgr0)\]"
 
 # several prompts created by me.
 alias promptnormal='PS1="\u@\h \w\$"'
-alias huh='PS1="\[$(tput bold)\]huh? \[$(tput sgr0)\]"'
-alias promptmaster='PS1="\[$(tput bold)\]Yes, master? \[$(tput sgr0)\]"'
-alias prompt2master='PS1="\[\e[1;34m\]\u@\h: \w\[$(tput sgr0)\]\$(__git_ps1)\n\[\e[1;36m\]Yes, master? \[\e[0m\]"'
+alias huh='PS1="$boldhuh? $normal"'
+alias promptmaster='PS1="$boldYes, master? $normal"'
+alias prompt2master='PS1="$thickblue\u@\h: \w$normal\$(__git_ps1)\n$thinblueYes, master? \[\e[0m\]"'
 alias promptgit='PS1="$thinblue\w$normal\$(__git_ps1)$\n"'
 
 # choose default prompt:
