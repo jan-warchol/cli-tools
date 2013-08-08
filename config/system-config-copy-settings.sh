3965ffff3faa553a548b5084420d8ba08b913b4a
Janek Warchoł <lemniskata.bernoullego@gmail.com>
2013-08-08 13:07:02 +0200

    config: redo copying config files, do backups

--- config/system-config-copy-settings.sh
+++ config/system-config-copy-settings.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
-# configure my user account. Run this once.
+# Copy my configuration files from cli-tools repository
+# to appropriate locations.  Can be ran multiple times.
 
-# create an .inputrc file and link to the content
-echo '$include $MY_CONFIGS/inputrc' | tee -a $HOME/.inputrc
+# TODO: describe what kind of stuff inputrc contains
+
+if [ -f ~/.inputrc ]; then
+    cp ~/.inputrc ~/.inputrc.backup
+fi
+cp $MY_CONFIGS/inputrc ~/.inputrc
+
+
+# copy my git settings.  When git > 1.7.10 becomes widespread,
+# this probably could be used instead:
+# [include]
+#	path = $MY_CONFIGS/gitconfig
+
+if [ -f ~/.gitconfig ]; then
+    cp ~/.gitconfig ~/.gitconfig.backup
+fi
+cp $MY_CONFIGS/gitconfig ~/.gitconfig
+
 
 # load my cron jobs
 # crontab $MY_CONFIGS/cron.jobs
 
 # make a backup of polish keyboard layout
 # sudo cp /usr/share/X11/xkb/symbols/pl /usr/share/X11/xkb/symbols/pl.backup
-
 # overwrite polish keyboard layout with my own layout
 # sudo cp -f $MY_CONFIGS/janek-keyboard-layout /usr/share/X11/xkb/symbols/pl
-
-# copy my global git settings
-cp $MY_CONFIGS/gitconfig ~/.gitconfig
-# when git > 1.7.10 becomes widespread, this could be used instead:
-# [include]
-#	path = $MY_CONFIGS/gitconfig
 
