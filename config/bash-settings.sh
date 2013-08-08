69b90afeb2cad8613a63ea509846810fb2bb2cd5
Janek Warchoł <lemniskata.bernoullego@gmail.com>
2013-08-08 10:21:37 +0200

    config: change command-line-stuff to cli-tools

--- config/bash-settings.sh
+++ config/bash-settings.sh
@@ -12,9 +12,9 @@
 export MY_DROPBOX="$ALL_MY_STUFF/Dropbox/"
 export MY_OWN_DATA="$ALL_MY_STUFF/moje/"
 export MY_REPOSITORIES="$ALL_MY_STUFF/repos/"
-export LILYPOND_BUILD_DIR=$ALL_MY_STUFF/lily-builds/
-export LILYPOND_GIT=$MY_REPOSITORIES/lilypond-git/
-export MY_CLI_TOOLS="$MY_REPOSITORIES/command-line-stuff/"
+export LILYPOND_BUILD_DIR="$ALL_MY_STUFF/lily-builds/"
+export LILYPOND_GIT="$MY_REPOSITORIES/lilypond-git/"
+export MY_CLI_TOOLS="$MY_REPOSITORIES/cli-tools/"
 export MY_SCRIPTS="$MY_CLI_TOOLS/scripts"
 export LILY_SCRIPTS="$MY_CLI_TOOLS/lilypond"
 
