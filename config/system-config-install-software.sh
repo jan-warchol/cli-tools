0d9274f7e2211acbb40035af12fb54d0ccf835ca
Janek Warchoł <lemniskata.bernoullego@gmail.com>
2013-08-08 15:09:05 +0200

    config: add warnigns when installation fails

--- config/system-config-install-software.sh
+++ config/system-config-install-software.sh
@@ -7,36 +7,38 @@ die() {
 
 # libraries for compiling git from source
 sudo apt-get install libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev
-sudo git clone git://git.kernel.org/pub/scm/git/git.git
+sudo git clone git://git.kernel.org/pub/scm/git/git.git \
+|| die "Failed to clone git sources"
 cd /repos/git
 sudo make prefix=/usr/local all
 sudo make prefix=/usr/local install
 sudo apt-get remove git
 
 # DVD decryption
-sudo apt-get install libdvdread4
-sudo /usr/share/doc/libdvdread4/install-css.sh
+sudo apt-get install libdvdread4 || die "Failed to download DVD decoders"
+sudo /usr/share/doc/libdvdread4/install-css.sh \
+|| die "Failed to install DVD decoders"
 
 # a program for changing line endings from windows to unix
-sudo apt-get install flip
+sudo apt-get install flip || die "Failed to install flip"
 
 # a program for changing screen brightness
-sudo apt-get install xbacklight
+sudo apt-get install xbacklight || die "Failed to install xbacklight"
 
 # a program for opening files with GUI programs from command line (gnome-open)
-sudo apt-get install libgnome2-0
+sudo apt-get install libgnome2-0 || die "Failed to install gnome-open"
 
 # install keepass2.  first two commands might not be necessary.
 #sudo apt-add-repository ppa:jtaylor/keepass
 #sudo apt-get update
-sudo apt-get install keepass2
+sudo apt-get install keepass2 || die "Failed to install KeePass"
 
-sudo apt-get install gimp
+sudo apt-get install gimp || die "Failed to install GIMP"
 
-sudo apt-get install imagemagick
+sudo apt-get install imagemagick || die "Failed to install ImageMagick"
 
-sudo apt-get install kate
+sudo apt-get install kate || die "Failed to install Kate"
 
-sudo apt-get install vlc
+sudo apt-get install vlc || die "Failed to install VLC"
 
 # TODO install frescobaldi
