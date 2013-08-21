42216db6c8d82a0c6cd68e7df72fad0e071330a7
Janek Warchoł <lemniskata.bernoullego@gmail.com>
2013-08-21 12:36:25 +0200

    add misc snippets

--- snippets/misc snippets.sh
+++ snippets/misc snippets.sh
@@ -1,5 +1,13 @@
+# see http://www.thegeekstuff.com/2010/05/bash-shell-special-parameters/
+
+# the path to the script:
+$0
+
 # n-th script argument:
 $1 $2 $3 
+
+# all arguments (w/o quotes there are problems with whitespace):
+"$@"
 
 # number of arguments:
 $#
