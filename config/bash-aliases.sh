7adb4eda274fc784a27c8c0fd1e8490e592be641
Janek Warchoł <lemniskata.bernoullego@gmail.com>
2013-08-21 12:53:49 +0200

    change 'lily' alias to a function

--- config/bash-aliases.sh
+++ config/bash-aliases.sh
@@ -56,13 +56,14 @@ alias lpull='$LILY_SCRIPTS/pull-all.sh; alert'
 alias fixcc='$LILYPOND_GIT/scripts/auxiliar/fixcc.py'
 alias flly='flip -ub *.ly; flip -ub *.ily'
 
-alias lgit='$LILYPOND_GIT/build/out/bin/lilypond'
-alias lily='$LILYPOND_BUILD_DIR/$defaultlily/out/bin/lilypond'
+lily() {
+    # doesn't support filenames with spaces.
+    files="${@#$1}"
+    $LILYPOND_BUILD_DIR/$1/out/bin/lilypond $files
+}
+alias lgit='$LILYPOND_GIT/build/out/bin/lilypond'glpp,
 alias lcurrent='$LILYPOND_BUILD_DIR/current/out/bin/lilypond'
 alias lmaster='$LILYPOND_BUILD_DIR/master/out/bin/lilypond'
-alias lstable='$LILYPOND_BUILD_DIR/stable/out/bin/lilypond'
-alias lrelease='$LILYPOND_BUILD_DIR/release/out/bin/lilypond'
-alias lstroke='$LILYPOND_BUILD_DIR/strokeadjust/out/bin/lilypond'
 
 alias lbuild='$LILY_SCRIPTS/build-lily.sh -t 2'
 alias mb='$LILY_SCRIPTS/build-lily.sh -t 0 -b; alert'
