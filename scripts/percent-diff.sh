c27f0bebdcf7ed80b825462ace1de1cb241d68a1
Janek Warchoł <lemniskata.bernoullego@gmail.com>
2013-09-09 12:14:24 +0200

    percent-diff: find bigger

--- scripts/percent-diff.sh
+++ scripts/percent-diff.sh
@@ -5,11 +5,20 @@ fileB="pies(2009-10-09) (copy).txt"
 
 A_len=$(cat "$fileA" | wc -l)
 B_len=$(cat "$fileB" | wc -l)
-max_len=$(($A_len>$B_len?$A_len:$B_len))
-min_len=$(($A_len<$B_len?$A_len:$B_len))
+if [[ $A_len>$B_len ]]; then
+    bigger=$fileA
+    smaller=$fileB
+    max_len=$A_len
+    min_len=$B_len
+else
+    bigger=$fileB
+    smaller=$fileA
+    max_len=$B_len
+    min_len=$A_len
+fi
 
 differences=$(sdiff -B -s "$fileA" "$fileB" | wc -l)
 common=$(expr $max_len - $differences)
 
 percentage=$(echo "100 * $common / $min_len" | bc)
-echo perc $percentage
+echo "$percentage% of file $smaller is the same as in $bigger"
