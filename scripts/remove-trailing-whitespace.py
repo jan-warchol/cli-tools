01f2252ec67d7128297097a45e7da6d63a326779
Janek Warchoł <lemniskata.bernoullego@gmail.com>
2013-09-24 00:44:33 +0200

    rm trailing: misc fixes

--- scripts/remove-trailing-whitespace.py
+++ scripts/remove-trailing-whitespace.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python
 
-# This script removes trailing whitepace from files.
+# This script removes trailing whitespace from files.
 # It doesn't remove trailing newlines; in fact, it makes
 # sure that at least one trailing newline is present.
 
-from sys import argv
 import os, sys
 
-# iterate through all arguments.  When the script is called
-# with a wildcard, for example 'rstrip *', the _shell_ will
-# expand the wildcard and pass all results to the script.
-for pathname in argv[1:]:
+# Iterate through all arguments.  When the script is called
+# with a wildcard (for example 'remove-trailing-whitespace.py *'),
+# it's the *shell* that will expand the wildcard, and pass all
+# resulting paths as arguments to the script.
+
+for pathname in sys.argv[1:]:
     if os.path.isfile(pathname):
         print 'Stripping trailing whitepace from', pathname
         f = file(pathname, 'r')
         s = "\n".join([l.rstrip() for l in f])
         f.close()
+
         f = file(pathname, 'w')
         f.write(s+"\n")
         f.close()
