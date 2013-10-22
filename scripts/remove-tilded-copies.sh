383186ba0a35870847006fa3a397bd3dad8a5a4f
Janek Warchoł <lemniskata.bernoullego@gmail.com>
2013-10-22 22:36:44 +0200

    remove tildes - only from current dir

--- scripts/remove-tilded-copies.sh
+++ scripts/remove-tilded-copies.sh
@@ -4,11 +4,10 @@
 
 IFS=$(echo -en "\n\b")
 
-cd $HOME
 for file in $(find -name "*~")
 do
     if [[ "$file" != *".local/share/Trash"* ]]; then
-        echo $file | tee -a ~/.deleted-tildes.log 
+        echo $file | tee -a .deleted-tildes.log
         mv $file ~/.local/share/Trash/
     fi        
 done
