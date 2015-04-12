b9fb03ca495c8a24484f5561667771c973416596
Janek Warchoł <lemniskata.bernoullego@gmail.com>
2015-04-12 13:57:12 +0200

    Rename LILY_SCRIPTS to LILYPOND_SCRIPTS

--- config/bash-aliases.sh
+++ config/bash-aliases.sh
@@ -1,6 +1,6 @@
 #### LILYPOND ALIASES:
 
-alias lpull='$LILY_SCRIPTS/pull-all.sh'
+alias lpull='$LILYPOND_SCRIPTS/pull-all.sh'
 alias fixcc='$LILYPOND_GIT/scripts/auxiliar/fixcc.py'
 
 lily() {
@@ -17,9 +17,9 @@ alias lgit='$LILYPOND_GIT/build/out/bin/lilypond'
 alias lcurrent='$LILYPOND_BUILD_DIR/current/out/bin/lilypond'
 alias lmaster='$LILYPOND_BUILD_DIR/master/out/bin/lilypond'
 
-alias lbuild='$LILY_SCRIPTS/build-lily.sh -t 2'
-alias mb='$LILY_SCRIPTS/build-lily.sh -t 0 -b; alert'
-alias mn='$LILY_SCRIPTS/build-lily.sh -t 0   ; alert'
-alias ms='$LILY_SCRIPTS/build-lily.sh -t 0 -s; alert'
+alias lbuild='$LILYPOND_SCRIPTS/build-lily.sh -t 2'
+alias mb='$LILYPOND_SCRIPTS/build-lily.sh -t 0 -b; alert'
+alias mn='$LILYPOND_SCRIPTS/build-lily.sh -t 0   ; alert'
+alias ms='$LILYPOND_SCRIPTS/build-lily.sh -t 0 -s; alert'
 
-alias lbuildtesting='$LILY_SCRIPTS/build-lily.sh -t 1 -c master -m "springs stroke cleaned-alignment add-dur-moll  thinner_skylines" -d testing'
+alias lbuildtesting='$LILYPOND_SCRIPTS/build-lily.sh -t 1 -c master -m "springs stroke cleaned-alignment add-dur-moll  thinner_skylines" -d testing'
